40fb327c36bc8b17fb3d4757c007a036f0acb7cb
kamindu nanayakkara <kamindu.nanayakkara@ebuilder.com>
2018-09-12 00:11:47 +0530

    module 1 update class

--- module1/src/main/java/com/demo/Class1.java
+++ module1/src/main/java/com/demo/Class1.java
@@ -2,4 +2,13 @@ package com.demo;
 
 public class Class1 {
 
+	private int value;
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public void setValue(int value) {
+		this.value = value;
+	}
 }
